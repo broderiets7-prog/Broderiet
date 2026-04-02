f039b6bb67b090c00bda4368ef4d660b1073476e
broderiets7-prog <broderiets7@gmail.com>
2026-04-02 08:02:54 +0200

    Update order.js

--- netlify/functions/order.js
+++ netlify/functions/order.js
@@ -12,7 +12,7 @@ exports.handler = async (event) => {
 
   const payload = JSON.stringify({
     from: "Bröderiet <order@broderiets.se>",
-    to: "order@broderiets.se",
+    to: "broderiets7@gmail.com",
     subject: `Ny beställning från ${name}`,
     html: `
       <h2>Ny beställning</h2>
